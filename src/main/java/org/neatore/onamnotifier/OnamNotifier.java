0b82a46b6e6c03794b1827d60a132e5193aa4fdc
Dongwan Kim <dongwan204@naver.com>
2026-07-25 15:29:45 +0900

    Initializing project

---
+++ src/main/java/org/neatore/onamnotifier/OnamNotifier.java
@@ -0,0 +1,11 @@
+package org.neatore.onamnotifier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnamNotifier {
+    public static void main(String[] args) {
+        SpringApplication.run(OnamNotifier.class, args);
+    }
+}
