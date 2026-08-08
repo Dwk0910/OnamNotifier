9c9c96195f57daf339abe2933e9621e152ce4ca1
Dongwan Kim <dongwan204@naver.com>
2026-08-09 05:06:01 +0900

    Feature: 'me' endpoint to check that the client is logged in

--- src/main/java/org/neatore/onamnotifier/OnamNotifier.java
+++ src/main/java/org/neatore/onamnotifier/OnamNotifier.java
@@ -1,11 +1,32 @@
 package org.neatore.onamnotifier;
 
+import lombok.RequiredArgsConstructor;
+
+import org.neatore.onamnotifier.service.TokenService;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class OnamNotifier {
     public static void main(String[] args) {
         SpringApplication.run(OnamNotifier.class, args);
+    }
+}
+
+@RestController
+@RequiredArgsConstructor
+class Controller {
+    private final TokenService tokenService;
+
+    @GetMapping("/me")
+    public ResponseEntity<String> me(@CookieValue("ONN_ACCESS") String token) {
+        return ResponseEntity.ok(
+                this.tokenService.getUserEmail(token)
+        );
     }
 }
